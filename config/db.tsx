455e102032fec5e25097c3961ba1159c5e8657da
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-10-03 20:30:54 +0200

    Delete .config inside GITIGNORE & add Database Connection

---
+++ config/db.tsx
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+mongoose
+    .connect('mongodb+srv://'
+    + process.env.GROUP
+    + ':'
+    + process.env.PASSWORD
+    + '@'
+    + process.env.DATABASE
+    + '/'
+    + process.env.PROJECT
+    )
+    .then(() => {
+        console.log("[server]: Connect to MongoDB");
+      })
+    .catch((err) => {
+    console.log("[server]: Failed to connect to MongoDB", err);
+    });
